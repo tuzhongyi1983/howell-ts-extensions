383a3680b2362c0aa63e1bd5354e5bfa3215ae80
tuzhongyi <tuzhongyi@howell.net.cn>
2019-11-27 14:16:44 +0800

    master

--- test/string-extensions.test.ts
+++ test/string-extensions.test.ts
@@ -2,10 +2,6 @@ import URL = require("url-parse");
 import "../src/string-extensions";
 test("String.AddQueryStringIfNeed", async () => {
   const base = new String("http://192.168.21.244:9000/Villages");
-  //const base = "howell/ver10/data_service/village_system/Villages";
-  "howell/ver10/data_service/village_system/Villages"
-    .addQueryStringIfNeed("name", "value")
-    .addQueryStringIfNeed("name", null);
   let s = base.addQueryStringIfNeed("name", "value");
   expect(s).toBe("http://192.168.21.244:9000/Villages?name=value");
   let s2 = s.addQueryStringIfNeed("age", 23);
